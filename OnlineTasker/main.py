4ae354dcca6bcea40120b0d971db032917787284
Виталий <144565815+vitaliysev@users.noreply.github.com>
2024-05-12 23:46:28 +0300

    Update main.py

--- OnlineTasker/main.py
+++ OnlineTasker/main.py
@@ -21,21 +21,26 @@ def form(request: Request, school: str = Form(...), semester: str = Form(...), s
 @app.post('/tasks/image')
 def form(request: Request, number: str = Form(...)):
     array_words = number.split(' ')
-    img = ''
+    print(array_words)
     if (array_words[0] == 'Matan'):
-        if (array_words[2][0] == 'T'):
-            img = f'Online-tasking-pages/calculus-tasking/out{dict_of_number_T_calculus[array_words[2]]}.png'
+        if (array_words[3][0] == 'T'):
+            img = f'../static/Online-tasking-pages/calculus-tasking/out{dict_of_number_T_calculus[int(array_words[1])][array_words[3]]}.png'
+            source = f'../static/Online-tasking-pages/calculus-tasking/'
         else:
-            img = f'Online-tasking-pages/calculus-tasks/out{dict_of_number_of_tasks_calculus[int(array_words[1])][array_words[2]]}.png'
+            img = f'../static/Online-tasking-pages/calculus-tasks/out{dict_of_number_of_tasks_calculus[int(array_words[2])][array_words[3]]}.png'
+            source = f'../static/Online-tasking-pages/calculus-tasks/'
     elif (array_words[0] == 'Angem'):
-        if (array_words[2][0] == 'T'):
-            img = f'Online-tasking-pages/analytic-geometry-tasking/out{dict_of_number_T_geometry[array_words[1]]}.png'
+        if (array_words[2][0] == 'Т'):
+            img = f'../static/Online-tasking-pages/analytic-geometry-tasking/out{dict_of_number_T_geometry[int(array_words[1])][array_words[2]]}.png'
+            source = f'../static/Online-tasking-pages/analytic-geometry-tasking/'
         else:
-            img = f'Online-tasking-pages/analytic-geometry-tasks/out{dict_of_number_of_tasks_geometry[array_words[1]]}.png'
+            img = f'../static/Online-tasking-pages/analytic-geometry-tasks/out{dict_of_number_of_tasks_geometry[array_words[2]]}.png'
+            source = f'../static/Online-tasking-pages/analytic-geometry-tasks/'
     elif (array_words[0] == 'Discr'):
-        img = f'Online-tasking-pages/Discrete-math-tasks/out{dict_of_number_of_tasks_discret[array_words[1]]}.png'
-    return templates.TemplateResponse('third_page.html', context={'request': request, 'number' : number})
+        img = f'../static/Online-tasking-pages/Discrete-math-tasks/out{dict_of_number_of_tasks_discret[array_words[1]]}.png'
+        source = f'../static/Online-tasking-pages/Discrete-math-tasks/'
+    return templates.TemplateResponse('third_page.html', context={'request': request, 'image': img, 'source':source})
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='127.0.0.1', port=8010, reload=True)
+    uvicorn.run("main:app", host='127.0.0.1', port=8013, reload=True)
